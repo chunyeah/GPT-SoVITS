046974fd2707d2801c1cacc0cd690706908d9ba7
chun <yechunjun@gmail.com>
2024-08-27 17:38:00 +0800

    feat: update download.sh

---
+++ Docker/gpt.sh
@@ -0,0 +1,8 @@
+wget https://uproai.blob.core.windows.net/models/原神/枫原万叶/枫原万叶-e10.ckpt
+wget https://uproai.blob.core.windows.net/models/原神/纳西妲/纳西妲-e10.ckpt
+wget https://uproai.blob.core.windows.net/models/原神/胡桃/胡桃-e10.ckpt
+wget https://uproai.blob.core.windows.net/models/原神/芙宁娜/芙宁娜-e10.ckpt
+wget https://uproai.blob.core.windows.net/models/原神/那维莱特/那维莱特-e10.ckpt
+wget https://uproai.blob.core.windows.net/models/原神/钟离/钟离-e10.ckpt
+wget https://uproai.blob.core.windows.net/models/原神/雷电将军/雷电将军-e10.ckpt
+wget https://uproai.blob.core.windows.net/models/原神/魈/魈-e10.ckpt
---
+++ Docker/sovits.sh
@@ -0,0 +1,8 @@
+wget https://uproai.blob.core.windows.net/models/原神/枫原万叶/枫原万叶_e10_s290.pth
+wget https://uproai.blob.core.windows.net/models/原神/纳西妲/纳西妲_e10_s400.pth
+wget https://uproai.blob.core.windows.net/models/原神/胡桃/胡桃_e10_s210.pth
+wget https://uproai.blob.core.windows.net/models/原神/芙宁娜/芙宁娜_e10_s290.pth
+wget https://uproai.blob.core.windows.net/models/原神/那维莱特/那维莱特_e10_s280.pth
+wget https://uproai.blob.core.windows.net/models/原神/钟离/钟离_e10_s1120.pth
+wget https://uproai.blob.core.windows.net/models/原神/雷电将军/雷电将军_e10_s210.pth
+wget https://uproai.blob.core.windows.net/models/原神/魈/魈_e10_s600.pth
